88783d2ad68947d056cb494bba21638c68002c3e
Chris Kelley <ckelley@redhat.com>
2021-05-14 17:22:46 +0100

    Sonar test

---
+++ src/main/java/org/mozilla/jss/pkcs11/myJavaClass.java
@@ -0,0 +1,16 @@
+package org.mozilla.jss.pkcs11;
+
+public class myJavaClass {
+
+    static final public String string = new String();
+    static final public String string2 = new String();
+
+    private int camel_case_for_no_reason() {
+        if (string.length() == 0) {
+            return 3;
+        } else if (string.length() == 0) {
+            return 2;
+        }
+        return 0;
+    }
+}
